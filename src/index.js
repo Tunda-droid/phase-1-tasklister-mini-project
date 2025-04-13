db79b683cacc257aef135994de576f2c763fb246
Edgar Mikuro <edgarmikuro@gmail.com>
2025-04-13 10:30:07 +0300

    Completes a tasklister mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById("create-task-form");
+const taskList = document.getElementById("tasks");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const input = document.getElementById("new-task-description");
+  const task = input.value;
+
+  const li = document.createElement("li");
+  li.textContent = task;
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "❌";
+  deleteBtn.addEventListener("click", () => li.remove());
+
+  li.appendChild(deleteBtn);
+  taskList.appendChild(li);
+
+  input.value = ""; // clear input after adding
+});
+
 });
